31819c6abf75006e79cbdde6574508e93cbfeb5e
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-02 07:16:47 +0500

    fix: correct date calculation to subtract 6 minutes instead of 13 days

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().subtract(13, "days").format();
+const date = moment().subtract(6, 'minutes').format();
 
 const data = {
   date: date,
