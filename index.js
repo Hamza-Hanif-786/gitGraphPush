8be9c80855a3556a9fa40edd1879283c4c59bde0
Hamza Hanif <hamzahanif1024@gmail.com>
2025-05-04 07:30:13 +0500

    Fix date calculation to subtract 15 minutes before formatting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().subtract(27, "minutes").format();
+const date = moment().subtract(15, "minutes").format();
 
 const data = {
   date: date,
